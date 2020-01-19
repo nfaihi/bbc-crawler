93b1160a304bde1b1b67fd8f808f5fbac94dd8a7
Ahmed NFAIHI <anfaihi10@gmail.com>
2020-01-19 17:55:58 +0100

    feature : insert data into a local mongodb database

--- newscrawler/newscrawler/pipelines.py
+++ newscrawler/newscrawler/pipelines.py
@@ -4,8 +4,30 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+import json
 
 
 class NewscrawlerPipeline(object):
+    def __init__(self):
+        # create a connection to mongodb
+        self.connection = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+
+        # create the database
+        db = self.connection['news']
+        # create a collection into the previous database
+        self.collection = db['articles']
+
     def process_item(self, item, spider):
+        # write the received data into a json file
+        # to compare it with the one created while scraping
+        with open('items.json', 'w') as f:
+            f.write(json.dumps(item))
+
+        # insert the data into a mongodb database
+        for i in range(0, len(item)):
+            self.collection.insert(dict(item[str(i)]))
         return item
--- newscrawler/newscrawler/spiders/articlescrawler.py
+++ newscrawler/newscrawler/spiders/articlescrawler.py
@@ -9,7 +9,7 @@ class ArticlesSpider(scrapy.Spider):
     urls = []
     # news articles data
     articles = []
-
+    completed = False
     # select the news articles url
     start_urls = [
         'https://www.bbc.com/news'
@@ -17,19 +17,23 @@ class ArticlesSpider(scrapy.Spider):
 
     def parse(self, response):
         # select all news articles in the home page
-        for article in response.css('a.gs-c-promo-heading'):
-            article_url = article.css('::attr(href)').get()
-            # filter the necessary urls
-            if article_url.find('www.bbc') == -1 and article_url.count('/') == 2:
-                article_url = article_url.replace('/news', '', 1)
-                # generate valid urls and add them to the urls list
-                self.urls.append(self.start_urls[0] + article_url)
+        if self.completed is False:
+            for article in response.css('a.gs-c-promo-heading'):
+                article_url = article.css('::attr(href)').get()
+                # filter the necessary urls
+                if article_url.find('www.bbc') == -1 and article_url.count('/') == 2:
+                    article_url = article_url.replace('/news', '', 1)
+                    # generate valid urls and add them to the urls list
+                    self.urls.append(self.start_urls[0] + article_url)
+                    self.urls = list(dict.fromkeys(self.urls))
 
-        # scrap articles data
-        for url in self.urls:
-            if url is not None:
-                # url = response.urljoin(url)
-                yield scrapy.Request(url=url, callback=self.parse)
+            # scrap articles data
+            for url in self.urls:
+                if url is not None:
+                    # url = response.urljoin(url)
+                    yield scrapy.Request(url=url, callback=self.parse)
+            self.completed = True
+
         if response.css('div.story-body h1::text').get() is not None:
             text = ""
             # get article text
@@ -39,7 +43,6 @@ class ArticlesSpider(scrapy.Spider):
             # select the author if exist
             author = 'Unknown' if (response.css('span.byline__name::text').get() is None) else response.css('span.byline__name::text').get()
             # add the important data in a list as dictionaries
-            # this data must be stored in a mongodb database
             self.articles.append(dict({
                 'url': response.request.url,
                 'author': author,
@@ -48,6 +51,19 @@ class ArticlesSpider(scrapy.Spider):
                 'story_body': response.css('p.story-body__introduction::text').get(),
                 'article_text': text
             }))
-            # write selected articles in a json file
-            with open('articles.json', 'w') as f:
-                f.write(json.dumps(self.articles))
+
+        # prepare a dictionary of articles to use it while
+        # inserting data into database
+        i = 0
+        articles = {}
+        for article in self.articles:
+            articles.update({str(i): article})
+            i += 1
+
+        # write selected articles in a json file
+        with open('articles.json', 'w') as f:
+            f.write(json.dumps(articles))
+
+        # select the first 25 articles for testing
+        if len(articles) == 25:
+            yield articles
